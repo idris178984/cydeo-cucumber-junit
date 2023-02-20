dfe647bdd96eab6741da4d322d46409cfe4ac0d0
IdrisAkca <eylul178984@gmail.com>
2023-02-20 23:38:59 +0300

    Hooks

--- src/test/java/com/cydeo/step_definitions/Hooks.java
+++ src/test/java/com/cydeo/step_definitions/Hooks.java
@@ -5,8 +5,12 @@ In the class we will be able to pass pre- & post- conditions to
  each scenario and each step
  */
 
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
+
+
+import com.cydeo.utilities.Driver;
+import io.cucumber.java.*;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks {
 
@@ -26,20 +30,36 @@ public class Hooks {
         System.out.println("====this will only apply to scenarios with @db tag");
     }
     @After
-    public void teardownScenario() {
-        System.out.println("====Closing browser using cucumber @After");
-        System.out.println("====Scenario ended/ Take screenshot if failed!");
+    public void teardownScenario(Scenario scenario) {
+
+        //scenario.isFailed() --> if scenario fails this method will return TRUE boolean value
+
+        if (scenario.isFailed()){
+
+            byte [] screenshot = ((TakesScreenshot)Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName());
+
+        }
+
+
+
+        Driver.closeDriver();
+
+       // System.out.println("====Closing browser using cucumber @After");
+       // System.out.println("====Scenario ended/ Take screenshot if failed!");
 
     }
 
-    // @BeforeStep
+     @BeforeStep
     public void setupStep(){
         System.out.println("--------> applying setup using @BeforeStep");
     }
 
-    //@AfterStep
+    @AfterStep
     public void afterStep(){
         System.out.println("--------> applying tearDown using @AfterStep");
+
     }
+
 
     }
